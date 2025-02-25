d5aa3d7a0c6e04d925de372fa47ca388efcf9de4
Hanák Renáta <hanakrenata@ktch.hu>
2025-02-25 11:09:58 +0100

    Nap vége

---
+++ jari/src/Honda.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Honda(ArrayList<Vehicle> list) {
+    public void hondaData(){
+        for(vehicle : VehicleLIst){
+            if(vehicle.brand == "Honda")
+            System.out.println(vehicle);
+        }
+    }
+}
