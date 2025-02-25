473e0fbf1ce2c59211fb751183fa5900c43783d5
Ewemor Martin <martinewemor@gmail.com>
2025-02-25 11:05:35 +0100

    Óra vége

--- jari/src/Vehicle.java
+++ jari/src/Vehicle.java
@@ -1,5 +1,14 @@
+
+import java.time.LocalDate;
+
 public class Vehicle {
-    
+    String rendszam;
+    String marka;
+    Integer ev;
+    String uzemanyag;
+    Integer ar;
+    boolean klima;
+    LocalDate datum;
 
    
 }
